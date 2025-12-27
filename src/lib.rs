9e18f874b04fe7c2acc6cbdb54c790463ef72d54
Baily <blitheshuo@gmail.com>
2025-12-27 11:17:52 -0500

    Add LeetCode 2236: Root Equals Sum of Children

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub struct Solution;
 
 // Add your solution modules here
+pub mod p2235_add_two_integers;
 pub mod p2413_smallest_even_multiple;
 pub mod p2469_convert_the_temperature;
-pub mod p2235_add_two_integers;
 pub mod p34_find_first_and_last_position_of_element_in_sorted_array;
 pub mod p35_search_insert_position;
 pub mod p704_binary_search;
+pub mod p2236_root_equals_sum_of_children;
