53e3c5bd8a38daf669898dd424003d2f3eee7339
Baily <blitheshuo@gmail.com>
2026-01-02 01:28:42 -0500

    1534 use filter to improve memory usage

--- src/p1534_count_good_triplets.rs
+++ src/p1534_count_good_triplets.rs
@@ -2,7 +2,21 @@ use crate::Solution;
 
 // @leet start
 impl Solution {
-    pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {}
+    pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
+        let mut counter = 0;
+        let n = arr.len();
+        for i in 0..n {
+            for j in (i + 1)..n {
+                if (arr[i] - arr[j]).abs() <= a {
+                    for k in (j + 1)..n {
+                        if (arr[j] - arr[k]).abs() <= b && (arr[i] - arr[k]).abs() <= c {
+                            counter += 1;
+                        }
+                    }
+                }
+            }
+        }
+        counter
+    }
 }
 // @leet end
-
