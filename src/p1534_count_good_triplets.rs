c506956f540d9123a3515dddf730c91e74064d9f
Baily <blitheshuo@gmail.com>
2026-01-01 23:45:54 -0500

    1534 iter

--- src/p1534_count_good_triplets.rs
+++ src/p1534_count_good_triplets.rs
@@ -2,8 +2,7 @@ use crate::Solution;
 
 // @leet start
 impl Solution {
-    pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
-        
-    }
+    pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {}
 }
 // @leet end
+
