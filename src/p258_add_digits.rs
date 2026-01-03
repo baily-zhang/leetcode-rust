0f39b139222d1e9430119d7bb0189fa6832eca8a
Baily <blitheshuo@gmail.com>
2026-01-03 10:52:34 -0500

    solution for 258 add digits

--- src/p258_add_digits.rs
+++ src/p258_add_digits.rs
@@ -3,7 +3,15 @@ use crate::Solution;
 // @leet start
 impl Solution {
     pub fn add_digits(num: i32) -> i32 {
-        
+        let mut n = num;
+        while n >= 10 {
+            n = n
+                .to_string()
+                .chars()
+                .fold(0, |acc, a| acc + a.to_digit(10).unwrap() as i32)
+        }
+        n
     }
 }
 // @leet end
+
