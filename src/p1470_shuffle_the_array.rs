884fe580b8f0b3a8ee05e502f692ebc26b05bff3
Baily <blitheshuo@gmail.com>
2026-01-14 23:57:39 -0500

    1470 solved

--- src/p1470_shuffle_the_array.rs
+++ src/p1470_shuffle_the_array.rs
@@ -3,14 +3,8 @@ use crate::Solution;
 // @leet start
 impl Solution {
     pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
-        let mut result = Vec::new();
-
-        for i in 0..n as usize {
-            result.push(nums[i]);
-            result.push(nums[i + n as usize]);
-        }
-        result
+        let n = n as usize;
+        (0..n).flat_map(|i| [nums[i], nums[i + n]]).collect()
     }
 }
 // @leet end
-
