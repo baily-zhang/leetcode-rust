9e397eae4d93cba563f88cc915b50b5ad5b32ecd
Baily <blitheshuo@gmail.com>
2026-01-19 00:49:20 -0500

    832 solved

--- src/p832_flipping_an_image.rs
+++ src/p832_flipping_an_image.rs
@@ -4,16 +4,9 @@ use crate::Solution;
 impl Solution {
     pub fn flip_and_invert_image(image: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         image
-            .into_iter() // 1. Manager: "Here is a conveyor belt of Rows."
-            .map(|row| {
-                // 2. "For each Row..."
-                row.into_iter() // 3. Worker: "Unpack the pixels."
-                    .rev() // 4. "Reverse the pixels (Horizontal Flip)."
-                    .map(|x| 1 ^ x) // 5. "Invert the pixel."
-                    .collect() // 6. "Repackage into a new Row."
-            })
-            .collect() // 7. Manager: "Stack all the new Rows."
+            .into_iter()
+            .map(|row| row.into_iter().rev().map(|i| 1 ^ i).collect())
+            .collect()
     }
 }
 // @leet end
-
