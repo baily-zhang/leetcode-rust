ce6f11cc6d188242376d5f3203c08be129c7f41e
Baily <blitheshuo@gmail.com>
2026-01-20 23:26:13 -0500

    344 solved

--- src/p344_reverse_string.rs
+++ src/p344_reverse_string.rs
@@ -3,12 +3,21 @@ use crate::Solution;
 // @leet start
 impl Solution {
     pub fn reverse_string(s: &mut Vec<char>) {
-        let n = s.len();
-        for i in 0..n / 2 {
-            // Swap index 'i' with index 'n - 1 - i'
-            s.swap(i, n - 1 - i);
+        let len = s.len();
+        if len < 2 {
+            return;
         }
+
+        // 1. The Split
+        let (left_half, right_half) = s.split_at_mut(len / 2);
+
+        // 2. The Iterator Chain
+        left_half
+            .iter_mut()
+            .zip(right_half.iter_mut().rev())
+            .for_each(|(a, b)| {
+                std::mem::swap(a, b);
+            });
     }
 }
 // @leet end
-
