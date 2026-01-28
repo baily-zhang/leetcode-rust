6dac6f7a404bf9b9872c3ad8f55fd1e9c8c07dfa
Baily <blitheshuo@gmail.com>
2026-01-27 23:50:01 -0500

    1 challenge refreshing

--- src/p1_two_sum.rs
+++ src/p1_two_sum.rs
@@ -2,13 +2,16 @@ use crate::Solution;
 // @leet start
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        nums.iter().enumerate().find_map(|(i, &a)| {
-            nums.iter()
-                .enumerate()
-                .skip(i + 1)
-                .find(|(_, &b)| a + b == target)
-                .map()
-        })
+        nums.iter()
+            .enumerate()
+            .find_map(|(i, &a)| {
+                nums.iter()
+                    .enumerate()
+                    .skip(i + 1)
+                    .find(|&(_, &b)| a + b == target)
+                    .map(|(j, _)| vec![i as i32, j as i32])
+            })
+            .unwrap_or_default()
     }
 }
 // @leet end
