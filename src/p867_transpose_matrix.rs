41a092edea5b47a3089af12f3257a3fec840f396
Baily <blitheshuo@gmail.com>
2026-01-31 23:50:15 -0500

    867 challenge refreshing

--- src/p867_transpose_matrix.rs
+++ src/p867_transpose_matrix.rs
@@ -5,7 +5,7 @@ impl Solution {
     pub fn transpose(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let n = matrix[0].len();
         (0..n)
-            .map(|j| matrix.iter().map(|row| row[j]).collect())
+            .map(|t| matrix.iter().map(|row| row[t]).collect())
             .collect()
     }
 }
