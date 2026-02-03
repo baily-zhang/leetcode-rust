3cbb6b6724b143dca04a1a8750eeacb0ba26c351
Baily <blitheshuo@gmail.com>
2026-02-02 22:16:50 -0500

    867 challenge refreshing

--- src/p867_transpose_matrix.rs
+++ src/p867_transpose_matrix.rs
@@ -3,8 +3,8 @@ use crate::Solution;
 // @leet start
 impl Solution {
     pub fn transpose(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let r = matrix[0].len();
-        (0..r)
+        let n = matrix[0].len();
+        (0..n)
             .map(|j| matrix.iter().map(|row| row[j]).collect())
             .collect()
     }
