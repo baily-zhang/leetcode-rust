6ac00bf0144cc8796a18ca868955ebfcd2a68aeb
Baily <blitheshuo@gmail.com>
2026-02-04 23:43:16 -0500

    1 challenge refreshing

--- src/p1_two_sum.rs
+++ src/p1_two_sum.rs
@@ -4,14 +4,14 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         nums.iter()
             .enumerate()
-            .find_map(|(i, &a)| {
+            .find_map(|(j, &a)| {
                 nums.iter()
                     .enumerate()
-                    .skip(i + 1)
+                    .skip(j + 1)
                     .find(|&(_, &b)| a + b == target)
-                    .map(|(j, _)| vec![i as i32, j as i32])
+                    .map(|(i, _)| vec![j as i32, i as i32])
             })
-            .unwrap_or_default()
+            .unwrap()
     }
 }
 // @leet end
