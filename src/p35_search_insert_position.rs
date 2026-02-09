d36317a15716fed6e1485057f7f691f39b2568c5
Baily <blitheshuo@gmail.com>
2026-02-08 23:26:09 -0500

    35 challenge refreshing

--- src/p35_search_insert_position.rs
+++ src/p35_search_insert_position.rs
@@ -5,16 +5,6 @@ use crate::Solution;
 
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        let (mut left, mut right) = (0, nums.len());
-
-        while left < right {
-            let mid = left + (right - left) / 2;
-            if nums[mid] < target {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        left as i32
+        nums.iter().position()
     }
 }
