41a1ff87e20317a2214800aa5ca65e44652dba1f
Baily <blitheshuo@gmail.com>
2026-03-10 12:56:23 -0400

    1&867 challenge refreshing

--- src/p1_two_sum.rs
+++ src/p1_two_sum.rs
@@ -4,11 +4,11 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         nums.iter()
             .enumerate()
-            .find_map(|(i, &a)| {
+            .find_map(|(i, a)| {
                 nums.iter()
                     .enumerate()
                     .skip(i + 1)
-                    .find(|&(_, &b)| a + b == target)
+                    .find(|&(_, b)| a + b == target)
                     .map(|(j, _)| vec![i as i32, j as i32])
             })
             .unwrap()
