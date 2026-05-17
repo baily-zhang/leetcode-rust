199ec7f43653d70713a942a854e617feab9c1129
Baily <blitheshuo@gmail.com>
2026-05-17 18:41:09 -0400

    q1 refreashing

--- src/p1071_greatest_common_divisor_of_strings.rs
+++ src/p1071_greatest_common_divisor_of_strings.rs
@@ -5,4 +5,3 @@ impl Solution {
     pub fn gcd_of_strings(str1: String, str2: String) -> String {}
 }
 // @leet end
-
--- src/p1_two_sum.rs
+++ src/p1_two_sum.rs
@@ -1,19 +1,19 @@
 use crate::Solution;
 // @leet start
+use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        nums.iter()
-            .enumerate()
-            .find_map(|(i, a)| {
-                nums.iter().enumerate().skip(i + 1).find_map(|(j, b)| {
-                    if a + b == target {
-                        Some(vec![i as i32, j as i32])
-                    } else {
-                        None
-                    }
-                })
-            })
-            .unwrap()
+        let mut seen: HashMap<i32, i32> = HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            let complement = target - num;
+
+            if let Some(&j) = seen.get(&complement) {
+                return vec![j, i as i32];
+            }
+
+            seen.insert(num, i as i32);
+        }
+        vec![]
     }
 }
 // @leet end
