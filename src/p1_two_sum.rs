a536a3bfc7fe62990b13f7512630805aa7f66a44
Baily <blitheshuo@gmail.com>
2026-05-18 15:35:54 -0400

    q1 refreashing

--- src/p1_two_sum.rs
+++ src/p1_two_sum.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut seen: HashMap<i32, i32> = HashMap::new();
+
         for (i, &num) in nums.iter().enumerate() {
             let complement = target - num;
 
